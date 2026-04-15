2c5ccc2da5335223602909ad3a45fd0bb9f039ab
pelzmagic <akinloyepelz@gmail.com>
2026-04-15 12:11:48 +0100

    feat:add invoice table and create invoice modal

---
+++ services/apis/invoices.api.ts
@@ -0,0 +1,21 @@
+import { Api } from "./api";
+import { GetDoctorInvoices, GetDoctorInvoicesParams } from "@/types/invoices";
+
+export const getDoctorInvoicesApi = ({
+  page = "1",
+  limit = "10",
+  user_id,
+}: GetDoctorInvoicesParams) => {
+  const params: Record<string, string> = {
+    page,
+    limit,
+  };
+
+  if (user_id) params.user_id = user_id;
+
+  const queryString = new URLSearchParams(params).toString();
+
+  const url = `/doctor/invoices/${queryString ? `?${queryString}` : ""}`;
+
+  return Api.get<GetDoctorInvoices>(url, true);
+};
