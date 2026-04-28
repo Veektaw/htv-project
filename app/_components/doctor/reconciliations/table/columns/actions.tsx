d72b337c6e3dad0a3aa6ab0fca0edfc5a1b1a76d
pelzmagic <akinloyepelz@gmail.com>
2026-04-28 07:17:55 +0100

    fix:disable create invoice for pending reconciliations

--- app/_components/doctor/reconciliations/table/columns/actions.tsx
+++ app/_components/doctor/reconciliations/table/columns/actions.tsx
@@ -11,7 +11,9 @@ export default function Actions({
   reconciliation: Reconciliation;
 }) {
   const canCreateInvoice =
-    reconciliation.status !== "completed" && reconciliation.status !== "void";
+    reconciliation.status !== "completed" &&
+    reconciliation.status !== "void" &&
+    reconciliation.status !== "pending";
 
   return (
     <PopoverContent className="rounded-base w-fit p-2">
