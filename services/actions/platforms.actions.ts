5138b231786cc88825d5a2068d6bc5f6b4aa0948
TPriince <tobiloba110@gmail.com>
2026-06-04 15:02:15 +0100

    feat: add advanced settings management for platforms
    - Implemented AdvancedSettingsSkeleton component for loading states.
    - Created AdvancedSettingsWrapper to fetch all platforms and handle loading errors.
    - Developed AdvancedSettings component to manage platform configurations with form validation.
    - Introduced useUpdateAdvanceSettings hook for platform CRUD operations.
    - Enhanced advancedSettingsSchema to validate platform and brand partner pairs.
    - Updated platforms.actions to include admin-specific actions for creating, updating, and deleting platforms.
    - Added new API endpoints for managing platforms in the admin dashboard.
    - Refactored existing code for better clarity and maintainability.

--- services/actions/platforms.actions.ts
+++ services/actions/platforms.actions.ts
@@ -1,38 +1,17 @@
 "use server";
 
-import { CreatePlatformPayload } from "@/types/platforms";
 import { getUser } from "../auth";
 import {
+  adminCreatePlatformApi,
+  adminDeletePlatformApi,
+  adminUpdatePlatformApi,
   createPlatformApi,
-  getPlatformsApi,
   updatePlatformApi,
 } from "../apis/platforms.api";
-
-export const getPlatformsAction = async () => {
-  const user = await getUser();
-
-  if (!user) {
-    return { error: true, message: "User session not found", platforms: [] };
-  }
-
-  const response = await getPlatformsApi(user.id);
-  console.log({ createPRes: response.body });
-  console.log({ response: response.body }, "this is the response body");
-
-  if (!response.ok) {
-    return {
-      error: true,
-      message: response.body.message,
-      platforms: [],
-    };
-  }
-
-  return {
-    error: false,
-    message: "Platforms fetched successfully",
-    platforms: response.body.platforms,
-  };
-};
+import {
+  AdminCreatePlatformPayload,
+  CreatePlatformPayload,
+} from "@/types/platforms";
 
 export const createPlatformAction = async (payload: CreatePlatformPayload) => {
   const user = await getUser();
@@ -68,7 +47,43 @@ export const updatePlatformAction = async (
 
   const response = await updatePlatformApi(user.id, platformId, payload);
 
-  console.log({ updatePRes: response.body });
+  if (!response.ok) {
+    return {
+      error: true,
+      message: response.body.message,
+    };
+  }
+
+  return {
+    error: false,
+    message: "Platform updated successfully",
+  };
+};
+
+export const adminCreatePlatformAction = async (
+  data: AdminCreatePlatformPayload,
+) => {
+  const response = await adminCreatePlatformApi(data);
+
+  if (!response.ok) {
+    return {
+      error: true,
+      message: response.body.message,
+    };
+  }
+
+  return {
+    error: false,
+    message: "Platform created successfully",
+    data: response.body,
+  };
+};
+
+export const adminUpdatePlatformAction = async (
+  platformId: string,
+  data: AdminCreatePlatformPayload,
+) => {
+  const response = await adminUpdatePlatformApi(platformId, data);
 
   if (!response.ok) {
     return {
@@ -80,5 +95,22 @@ export const updatePlatformAction = async (
   return {
     error: false,
     message: "Platform updated successfully",
+    data: response.body,
+  };
+};
+
+export const adminDeletePlatformAction = async (platformId: string) => {
+  const response = await adminDeletePlatformApi(platformId);
+
+  if (!response.ok) {
+    return {
+      error: true,
+      message: response.body.message,
+    };
+  }
+
+  return {
+    error: false,
+    message: "Platform deleted successfully",
   };
 };
